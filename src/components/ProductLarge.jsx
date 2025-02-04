48e999c63bea4cef5acae7cb22bb64d162d7ffca
Marcus Jeong <marcus.jeong96@gmail.com>
2025-02-04 12:40:02 -0500

    changed layout of product details page

--- src/components/ProductLarge.jsx
+++ src/components/ProductLarge.jsx
@@ -7,42 +7,61 @@ export default function ProductLarge (props) {
 
     // restructure so that the item is no longer on a card and can look more like a ecommerce section with upper and lower areas
     const productsItem = (
-        <div key={product.id} className='card col-6'>
-            <div className='card-body text-start'>
-                <h1 className='card-title'>{product.name}</h1>
-                <p className='card-text'>Description Short: {product.descriptionShort}</p>
-                <h3 className='card-text'>Price: ${product.price}</h3>
-                <div className='card-text'>
-                Sizes: {product.sizes.map(size => {
-                    if (size.small) {
-                        return <p key={size._id}>small</p>
-                    }
-                    })}
+        <>
+            <div className="row mb-5">
+                <div className="col">
+                    <div key={product.id} className='card'>
+                        <div className='card-body text-start'>
+                        <h1 className='card-title'>{product.name}</h1>
+                            <p className='card-text'>Description Short: {product.descriptionShort}</p>
+                            <h3 className='card-text'>Price: ${product.price}</h3>
+                            <div className='card-text'>
+                                Sizes: {product.sizes.map(size => {
+                                    if (size.small) {
+                                        return <p key={size._id}>small</p>
+                                    }
+                                    })}
+                            </div>
+                            <p className='card-text'>
+                                Colors: {product.colors.map(color => (
+                                    color.colorName
+                                ))}
+                            </p>
+                            
+                        </div>
+                    </div>
                 </div>
-                <p className='card-text'>
-                Colors: {product.colors.map(color => (
-                    color.colorName
-                ))}
-                </p>
-                <p className='card-text'>Description Long: {product.descriptionLong}</p>
-                <h4>Additional Details:</h4>
-                {product.additionalDetails.map(prompt => (
-                <ul className='list-group mb-3' key={prompt._id}>
-                    <li className='list-group-item'>materials: {prompt.materials}</li>
-                    <li className='list-group-item'>Recommended: {prompt.recommendedFor}</li>
-                    <li className='list-group-item'>Made In: {prompt.madeIn}</li>
-                    <li className='list-group-item'>Care: {prompt.care}</li>
-                    <li className='list-group-item'>Shipping: {prompt.shipping}</li>
-                </ul>
-                ))}
-                <p className='card-text'>Type: {product.type}</p>
-                <p className='card-text'>Materials: {product.materials.map(material => (
-                material.material1
-                ))}</p>
-                <p className='card-text'>Promotion: {product.promotion}%</p>
-                <p className='card-text'>Stock: {product.stock} units</p>
+                <div className="col">
+                    <p>Image carousel placeholder</p>
+                </div>
             </div>
-        </div>
+            <div className="row text-start">
+                <h4>Additional Details:</h4>
+                <div className="col">
+                    <p className='card-text'>Description Long: {product.descriptionLong}</p>
+                </div>
+                <div className="col">
+                    {product.additionalDetails.map(prompt => (
+                        <ul className='list-group mb-3' key={prompt._id}>
+                            <li className='list-group-item'>materials: {prompt.materials}</li>
+                            <li className='list-group-item'>Recommended: {prompt.recommendedFor}</li>
+                            <li className='list-group-item'>Made In: {prompt.madeIn}</li>
+                            <li className='list-group-item'>Care: {prompt.care}</li>
+                            <li className='list-group-item'>Shipping: {prompt.shipping}</li>
+                        </ul>
+                    ))}
+                </div>
+                <div className="col">
+                    <p className='card-text'>Type: {product.type}</p>
+                    <p className='card-text'>Materials: {product.materials.map(material => (
+                    material.material1
+                    ))}</p>
+                    <p className='card-text'>Promotion: {product.promotion}%</p>
+                    <p className='card-text'>Stock: {product.stock} units</p>
+                </div>
+            </div>
+        </>
+        
     )
 
     return (
