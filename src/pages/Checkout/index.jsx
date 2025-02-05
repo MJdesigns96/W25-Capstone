39c760e15eddaf25fbb8cf337b07778d41c7d1e5
Marcus Jeong <marcus.jeong96@gmail.com>
2025-02-05 14:13:24 -0500

    finished a way to show users cart information from local storage including subtotal, taxes, and total

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -16,32 +16,91 @@ export default function Checkout() {
         let strObjFromStorage = items.cart;
         let cartObj = JSON.parse(strObjFromStorage);
         useEffect(() => {
-            console.log(cartObj);
-            setCart([cartObj]);
+            let temp = [];
+            for (const [key, value] of Object.entries(cartObj)) {
+                temp.push(value);
+            }
+            setCart(temp);
         }, []);
-        cart.forEach(entry => {
-            console.log(entry)
-        })
-        userCart = (
-            <div>
-                temp
-            </div>
-        )
     }
+
+    let subtotal = 0;
+    let taxes = 0;
+    let shipping = 0;
+    let total = 0;
+
+    let checkoutItems = () => {
+        // get item details from localstorage
+        userCart = cart?.map(entry => (
+            <div className="row" key={entry.details.id}>
+                <div className="col"></div>
+                <div className="col">
+                    <h4>{entry.details.name}</h4>
+                    <p>${entry.details.price.toFixed(2)}</p>
+                    <h5>Quantity: {entry.qty}</h5>
+                </div>
+            </div>
+        ));
+        
+        //set subtotal
+        cart?.map(entry => 
+            subtotal += (entry.details.price * entry.qty)
+        );
+        taxes = subtotal * 0.13;
+        total = subtotal + shipping + taxes;
+    }    
+    checkoutItems();
 
     const clearLocal = () => {
         localStorage.clear();
     }
 
+    let rightSide;
+    if (items.cart === undefined) {
+        rightSide = (
+            <div>
+                <h2>There is nothing in your Cart</h2>
+            </div>
+        )
+    } else {
+        rightSide = (
+            <div className="col bg-secondary-subtle my-5">
+                <div className="row">
+                    {userCart}
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col">
+                        <input type="text" placeholder="Discount code or gift code" />
+                    </div>
+                    <div className="col">
+                        <button type="button">Apply</button>
+                    </div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col text-end mx-5">
+                        <h4>Subtotal: ${subtotal.toFixed(2)}</h4>
+                        <h4>Shipping: {
+                            shipping === 0 ? "Free" :
+                            "$ "+ shipping.toFixed(2)
+                        }</h4>
+                        <h4>Taxes: ${taxes.toFixed(2)}</h4>
+                        <h3 className="mt-4">Total: ${total.toFixed(2)}</h3>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <>
-            <h1>
-                Checkout
-            </h1>
-            <div>
-                {userCart}
+            <div className="row">
+                <div className="col my-5">
+                    <h3>Payment Details</h3>
+                </div>
+                {rightSide}
             </div>
-            <button type="button" className="btn btn-primary" onClick={clearLocal}>Clear Local</button>
+            <button type="button" className="btn btn-primary mb-5" onClick={clearLocal}>Clear Local</button>
         </>
     )
 }
