f8885c51926285c24d4d1c10ba509ddd64ce694e
Marcus <marcus.jeong96@gmail.com>
2025-02-18 11:41:53 -0500

    added a way to add the size selection to order

--- src/components/ProductsSmall.jsx
+++ src/components/ProductsSmall.jsx
@@ -7,19 +7,20 @@ export default function ProductSmall (props) {
 
     const addToCart = (entry) => {
         let cart = {};
-        console.log(entry);
+        //currently the localstorage is overwriting the cart
         if (localStorage.getItem('cart') === null) {
-            cart[entry.id] = { details: entry, qty: 1 };
+            cart[entry.id] = { details: entry, qty: 1, size: itemSize };
             const strObj = JSON.stringify(cart);
             localStorage.setItem('cart', strObj);
         } else {
             let strObjFromStorage = localStorage.getItem('cart');
             cart = JSON.parse(strObjFromStorage);
-            if (cart[entry.id] === undefined) {
-                cart[entry.id] = { details: entry, qty: 1 };
+            if (cart[entry.id] === undefined || cart[entry.id].size !== itemSize) {
+                cart[entry.id] = { details: entry, qty: 1, size: itemSize };
             } else {
                 cart[entry.id].qty++;
             }
+            console.log(cart);
             const strObj = JSON.stringify(cart);
             localStorage.setItem('cart', strObj);
         }
@@ -29,9 +30,15 @@ export default function ProductSmall (props) {
     let itemSize;
 
     const handleSizes = (e) => {
-        const { checked } = e.target;
-        let size = e.target.name;
-        console.log(size, checked);
+        //get the id for the radio button
+        let preSplit = e.target.id;
+        //split the id into characters
+        let split = preSplit.split("");
+        //remove the number at the end of id
+        split.pop();
+        //join the id back together to a string
+        let join = split.join("");
+        itemSize = join;
     }
 
     let count = 0;
@@ -49,31 +56,33 @@ export default function ProductSmall (props) {
             </Link>
             <div className="card-footer">
                 <div className='card-text'>
-                    Sizes: {entry.sizes.map(size => {
-                        let temp = [];
-                        for(const [key,value] of Object.entries(size)) {
-                            if (value && key !== "_id") {
-                                temp.push(key);
-                            }
-                        }
-                        count++;
-                        return temp.map(size => 
-                            (
-                                <h5>
-                                    {/* change type to radio and only allow 1 selection. then set the localstorage size to the selected size */}
-                                    <input type="checkbox" id={`${size}${count}`} name={`${size}${count}`} onChange={handleSizes} />
-                                    <span> </span>
-                                    <label htmlFor={`${size}${count}`}>{size}</label>
-                                </h5>
-                            )
-                        )
-                    })}
-                </div>
-                <p className='card-text'>Promotion: {entry.promotion}%</p>
-                <p className='card-text'>Stock: {entry.stock} units</p>
-                <button type='button' onClick={() => addToCart(entry)}>Add to Cart</button>
-            </div>
-            
+                    <form>
+                        <div className="card-text">
+                            Sizes: {entry.sizes.map(size => {
+                                let temp = [];
+                                for(const [key,value] of Object.entries(size)) {
+                                    if (value && key !== "_id") {
+                                        temp.push(key);
+                                    }
+                                }
+                                count++;
+                                return temp.map(size => 
+                                    (
+                                        <h5>
+                                            <input type="radio" id={`${size}${count}`} name={`item${count}`} onChange={handleSizes} />
+                                            <span> </span>
+                                            <label htmlFor={`${size}${count}`}>{size}</label>
+                                        </h5>
+                                    )
+                                )
+                            })}
+                        </div>
+                        <p className='card-text'>Promotion: {entry.promotion}%</p>
+                        <p className='card-text'>Stock: {entry.stock} units</p>
+                        <button type='button' onClick={() => addToCart(entry)}>Add to Cart</button>
+                    </form>
+                </div>               
+            </div>           
         </div>
     ));
 
